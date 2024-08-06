aecf5da561d37ced10dfa1a81ddec3d8741326c0
RoseannaGG <roseanna.gamlen.greene@gmail.com>
2024-08-06 16:12:07 +1200

    kinfin

--- 09_orthologID_blastpipeline/03_orthofinder_perplant.sh
+++ 09_orthologID_blastpipeline/03_orthofinder_perplant.sh
@@ -66,63 +66,9 @@ PATH=$PATH:/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/ort
 crtl x and then y then enter
 
 
-
-cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/Piper
-
-# investigate ref Pn23.504
-head -n 50 Piper_nigrum.fa
-
-">Pn23.504
-MMKWPLLSPIGFLFTLPIHIPRAKHGLPHFIRPASAFVDFEQTGELSSPSPLQLANLPLQLSRAELGPAD
-LSRAASGSFIGGPQYKLGTAFSAGFGSRGAVRRGPPTSCPVRSHLPSIVFAFTGLDMPPSLPGTPSAPP"
-
-# replace full stop with underscore Pn23_504
-cat Piper_nigrum.fa | sed 's/\./_/g' > PiperV4.fa
-
-# add | after Pn (Pn -> Pn|) Pn|23_504
- cat PiperV4.fa | sed 's/>Pn\(.*\)/>Pn|\1/g' > PiperV5.fa
-
-
-# investigate ref
-head -n 50 PiperV5.fa
-
-">Pn|23_504
-MMKWPLLSPIGFLFTLPIHIPRAKHGLPHFIRPASAFVDFEQTGELSSPSPLQLANLPLQLSRAELGPAD
-LSRAASGSFIGGPQYKLGTAFSAGFGSRGAVRRGPPTSCPVRSHLPSIVFAFTGLDMPPSLPGTPSAPP
-"
-
-### DASH
-# replace full stop with underscore Pn23_504
-cat Piper_nigrum.fa | sed 's/\./_/g' > PiperV4.fa ## BAD
-
-
-# add - after Pn (Pn -> Pn-) Pn-23_504
- cat PiperV4.fa | sed 's/>Pn\(.*\)/>Pn-\1/g' > Pn.fa
-
-
-############ UPATED
- ### pipe...
-
- cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2
-
-# replace . with _ for header title
- sed '/^>/ s/\./_/g' Piper_nigrum.fa > Pp.fa
-
- # remove . from in seq
- cat Pp.fa | sed 's/\.//g' > Ppp.fa 
-
-# add | after Pn (Pn -> Pn|) Pn|23_504
- cat Ppp.fa | sed 's/>Pn\(.*\)/>Pn|\1/g' > Pn.fa
-
- ## remove all * IN ALL FILES
- sed -i 's/\*//g' *.fa
-
-
-
-
 ### RENAME FILES AND HEADERS TO HOW I WANT THEM LATER!! 
 cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/
-mkdir renamedfa
+mkdir RenamedHeaders2
 
 cp *.fa ./renamedfa
 
@@ -148,48 +94,36 @@ for file in *; do
 done
 
 
-cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/renamedfa/* /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/renamedheaders/
 
 
-### Now rename all P1 and plant1 files to be P1| rather than P1_ in the headers...
+cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/Piper
 
-tail P4.fa
-">P4_43150
-FNYHLLAIALQVSISVQPLKVKNWILPEMPGLITDFLISLDDRFLYFVNWLHGDIRQYNI
-EDPTKPALAGQIWVGGLLQKGSNVFVITEDGQDFQFEVPEIQGHRLRGGPQMIQLSLDGK
-RLYVTNSLFSTWDRQFYPELVEKGAHMLQIDVDTEKGGLAINPNFFVDFGSEPDGPSLAH
-EMRYPGGDCTSDIWI*
-"
+# investigate ref Pn23.504
+head -n 50 Piper_nigrum.fa
 
-cat P4.fa | sed 's/_/|/g' > P4_test.fa
+# copy ref to new folder 
+cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/Piper/Piper_nigrum.fa /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2
+
+ cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2
+
+# replace . with _ for header title
+ sed '/^>/ s/\./_/g' Piper_nigrum.fa > Pp.fa
+
+ # remove . from in seq
+ cat Pp.fa | sed 's/\.//g' > Ppp.fa 
+
+# add | after Pn (Pn -> Pn|) Pn|23_504
+ cat Ppp.fa | sed 's/>Pn\(.*\)/>Pn|\1/g' > Pn.fa
+
+ ## remove all * IN ALL FILES
+ sed -i 's/\*//g' *.fa
 
 
 
-## works...now do as a loop and overwrite original
-
-for file in *.fa; do
-    sed -i 's/_/|/g' "$file"
-done
-
-">P4|43150
-FNYHLLAIALQVSISVQPLKVKNWILPEMPGLITDFLISLDDRFLYFVNWLHGDIRQYNI
-EDPTKPALAGQIWVGGLLQKGSNVFVITEDGQDFQFEVPEIQGHRLRGGPQMIQLSLDGK
-RLYVTNSLFSTWDRQFYPELVEKGAHMLQIDVDTEKGGLAINPNFFVDFGSEPDGPSLAH
-EMRYPGGDCTSDIWI*
-"
-
-## get correct version of Pn file in folder...
-rm -f Pn.fa
-
-cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/Piper/PiperV5.fa /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/renamedheaders/
-
-#rename Pn file...
-
-mv PiperV5.fa Pn.fa
 
 
 ## DOUBLE CHECK THERE'S ONLY THE VERSIONS OF THE FILES YOU WANT
-cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/renamedheaders
+cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder//RenamedHeaders2
 
 ls
 
@@ -198,75 +132,7 @@ rm P4_test.fa
 
 # run orthofinder on dataset that had been renamed
 
-cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/renamedheaders
-
-screen
-
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -t 64 -a 16 -T iqtree -M msa -A mafft -S diamond -I 1.5 -f /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/renamedheaders
-
-
-screen -r 163109
-
-"
-2024-07-12 22:48:35 : Done 96000 of 98152
-2024-07-12 22:48:45 : Done 97000 of 98152
-2024-07-12 22:48:54 : Done 98000 of 98152
-ERROR: Species tree inference failed
-ERROR: An error occurred, ***please review the error messages*** they may contain useful information about the problem.
-"
-
-
-## bad verison, forgot to delete P4_test.fa before running
-screen -r 268951
-
-
-# results here
-
-/Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/renamedheaders/OrthoFinder/
-
-
-## try again - this time only renamed Pn.fa... 
-
-screen
-
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -t 64 -a 16 -T iqtree -M msa -A mafft -S diamond -I 1.5 -f /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/headersrenamed
-
-screen -r 21893
-
-
-## also running this one... unsure what's different
-
-screen -r 163109
-
-
-### try again... but use "-" instead of "|" for Pn.fa
-
-
-cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed
-
-
-
-# add | after Pn (Pn -> Pn|) Pn|23_504
- cat Pn.fa | sed 's/|/-/g' > Pn.fa
-
-screen
-
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -t 64 -a 16 -T iqtree -M msa -A mafft -S diamond -I 1.5 -f /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed
-
-screen -r 36472
-
-
-
-
-
-## try again - accidentally edited ref 
-screen
-
-conda activate myenv
-
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -t 64 -a 16 -T iqtree -M msa -A mafft -S diamond -I 1.5 -f /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashtake2
-
-screen -r 184203
+cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder//RenamedHeaders2
 
 
 #### RenamedHeaders2
@@ -276,43 +142,13 @@ screen
 conda activate myenv
 
 
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -t 32 -a 16 -T iqtree -M msa -A mafft -S diamond -I 1.5 -f /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2
+/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -t 64 -a 16 -T iqtree -M msa -A mafft -S diamond -I 1.5 -f /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2
 
-screen -r 13426
+screen -r 336293
 
-
-
-### RUN ORTHOFINDER ON BLAST RESULTS
-
-## try again - accidentally edited ref 
-screen
-
-conda activate myenv
-
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -b /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashtake2/OrthoFinder/Results_Jul24/WorkingDirectory -t 32 -a 16 -T iqtree -M msa
-
-screen -r 13426
-
-### didn't work because of | characters
+# fucked up, forgot to delete Pn2.fa ... so rerun with bash but first edit speciesid file to have # in front of Pn2 (locally) then upload and replace that speciesid file in /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/WorkingDirectory/
 
 screen
 
-
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -b /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/headersrenamed/OrthoFinder/Results_Jul15/WorkingDirectory -t 64 -a 16 -T iqtree -M msa
-
-screen -r 209689
-
-
-
-## didn't work as Pn.fa was incorrect!!!
-screen
-
-
-/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -b /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/OrthoFinder/Results_Jul16/WorkingDirectory -t 64 -a 16 -T iqtree -M msa
-
-screen -r 21085
-
-# results
-
-/Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/OrthoFinder/Results_Jul16/WorkingDirectory/OrthoFinder/Results_Jul18_1/
-
+/Volumes/archive/kennylab/LocalProgrammes/Orthofinder/OrthoFinder/orthofinder -b /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/WorkingDirectory/ -t 32
+screen -r 46558
--- 09_orthologID_blastpipeline/04_kinfin.sh
+++ 09_orthologID_blastpipeline/04_kinfin.sh
@@ -32,106 +32,71 @@ cd kinfin
 
 
 
-    conda activate kinfin
-
 
 # make config file
 # https://kinfin.readme.io/docs/starting-from-a-clustering-files
 
-/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/generate_kinfin_input.py --fasta_dir /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/ orthofinder config
+conda activate kinfin
+
+
+/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/generate_kinfin_input.py --fasta_dir /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/ orthofinder config
 
 
 # copy ortho outputs needed into kinfin folder
-cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/
 
-mkdir blastpipeline
+# cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/Orthogroups/Orthogroups.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/
 
-cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/
-
-
-cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/OrthoFinder/Results_Jul16/WorkingDirectory/OrthoFinder/Results_Jul18_1/Orthogroups/Orthogroups.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/
-
-cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/OrthoFinder/Results_Jul16/WorkingDirectory/SpeciesIDs.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/
-
-cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/OrthoFinder/Results_Jul16/WorkingDirectory/SequenceIDs.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/
+cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/WorkingDirectory/OrthoFinder/Results_Aug05_1/Orthogroups/Orthogroups.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/
 
 
-cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/config.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/
+# edit Species IDs to have # in front of Pn2 as I fucked up... then replace species id file in this folder with this new one, then copy to kinfin folder
+
+ cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/WorkingDirectory/SpeciesIDs.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/
 
 
-cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/
+
+# cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/WorkingDirectory/SequenceIDs.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/
 
 
-## run...
+# make new seq ids as I fucked up... need to remove Pn2 seq ids...then copy newseqids file to kinfin folder
+cd /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/WorkingDirectory/
+cat SequenceIDs.txt | grep -v "^21_" >NewSeqIDs.txt
+
+
+
+cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/OrthoFinder/Results_Jul31/WorkingDirectory/NewSeqIDs.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/
+
+
+cp /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/config.txt /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/
+
+
+cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin
+
+
+## works
 screen
+conda activate kinfin
+/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/kinfin -g Orthogroups.txt -c config.txt -s NewSeqIDs.txt -p SpeciesIDs.txt -o test --fasta_dir /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/RenamedHeaders2/ 
 
-/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/kinfin -g Orthogroups.txt -c config.txt -s SequenceIDs.txt -p SpeciesIDs.txt -o test --fasta_dir /Volumes/archive/kennylab/roseanna_kawakawa/orthofinder/dashheaderrenamed/ 
-
-screen -r 90511
-
-
-
-
-#/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/kinfin -g Orthogroups.txt -c config.txt -s SequenceIDs.txt -p SpeciesIDs.txt --target_count --target_fraction --min --max --outprefix /Volumes/archive/kennylab/roseanna_kawakawa/kinfin --infer_singletons --min_proteomes --test --taxranks --repetitions --repetitions
-
-#$ /Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/kinfin -g Orthogroups.txt -c config.txt -s SequenceIDs.txt -p SpeciesIDs.txt --target_count --target_fraction --min --max --outprefix test --infer_singletons --min_proteomes --test
-
-
-":
-Traceback (most recent call last):
-  File "/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/src/kinfin.py", line 2305, in <module>
-    inputObj = InputObj(args)
-  File "/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/src/kinfin.py", line 2154, in __init__
-    self.check_fuzzy_count(args['--target_count'])
-  File "/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/src/kinfin.py", line 2270, in check_fuzzy_count
-    if int(target_count) > 0:
-ValueError: invalid literal for int() with base 10: '--target_fraction'
-(kinfin)
-"
-
-cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results
+screen -r 90308
 
 
 ## plot
-    conda activate kinfin
 
+conda activate kinfin
 
-/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/plot_cluster_size_distribution.py -i cluster_counts_by_taxon.txt -o /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/
-
-"$ /Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/plot_cluster_size_distribution.py -i cluster_counts_by_taxon.txt -o /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/
-[+] Start ...
-[+] Plotting "loglogpowerlaw" ...
-Traceback (most recent call last):
-  File "/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/plot_cluster_size_distribution.py", line 240, in <module>
-    dataObj.plot_cluster_sizes('loglogpowerlaw')
-  File "/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/plot_cluster_size_distribution.py", line 138, in plot_cluster_sizes
-    for proteome_count, x, y, y_bottom, colour in self.yield_counts_by_proteome_count('absolute'):
-  File "/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/plot_cluster_size_distribution.py", line 103, in yield_counts_by_proteome_count
-    cmap = mat.colormaps[self.cmap]
-AttributeError: module 'matplotlib' has no attribute 'colormaps'
-(kinfin)
-"
+/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/plot_cluster_size_distribution.py -i cluster_counts_by_taxon.txt 
 
 
 
 
+cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/test.kinfin_results/all/
 
-
-
-
-cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/
-
-grep -P "\t21" /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/all.all.cluster_1to1s.txt | cut -f1 > single_copy.cluster_ids.txt
+grep -P "\t21" /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/test.kinfin_results/all/all.all.cluster_1to1s.txt | cut -f1 > single_copy.cluster_ids.txt
 
 
 # get_protein_ids_from_cluster.py
 
-    conda activate kinfin
-
-
-cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/
-
-/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/get_protein_ids_from_cluster.py -g Orthogroups.txt --cluster_ids /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/single_copy.cluster_ids.txt -s -o /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/
 
 #I googled the error below (AttributeError: 'dict' object has no attribute 'iteritems'), and if you have a look at the results, it is a python2 =/= python3 problem (e.g. the first result is https://stackoverflow.com/questions/30418481/error-dict-object-has-no-attribute-iteritems)
 
@@ -229,40 +194,18 @@ conda activate myenv
 
 # extracting protein IDs from 
 
-cd /Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/blastpipeline/
-
- mkidr orthologue
-
 #Run the programme:
-cd /Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts
+cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/test.kinfin_results/
 
-## first backup the outputs from the other run
+mkdir proteinids
 
-cp *.txt backupoutputs
+cd /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/test.kinfin_results/proteinids
 
 
 # generates one file per orthologue Orthogroups.OG*.txt - and in each file there are the gene names from each plant that correspond to that orthologue gene (ie are the same gene) ... .
-./get_protein_ids_from_cluster.py -g /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/Orthogroups.txt --cluster_ids /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/single_copy.cluster_ids.txt 
-
-" ./get_protein_ids_from_cluster.py -g /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/Orthogroups.txt --cluster_ids /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/single_copy.cluster_ids.txt
-[+] Start ...
-[+] Parsing clusters in /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/single_copy.cluster_ids.txt ...
-[+] Parse groups /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/Orthogroups.txt ...
-[+] Found 100% of clusters ...
-[+] Writing files ...
-"
-
+/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/get_protein_ids_from_cluster.py -g /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/Orthogroups.txt --cluster_ids /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/test.kinfin_results/all/single_copy.cluster_ids.txt
 
 # generates single_copy.cluster_ids.protein_ids.txt - which is just a list of all the gene names in each plant that correspond to one of these orthologue clusters
-./get_protein_ids_from_cluster.py -g /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/Orthogroups.txt --cluster_ids /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/single_copy.cluster_ids.txt -s 
+/Volumes/archive/userdata/staff_users/roseannagamlen-greene/kinfin/scripts/get_protein_ids_from_cluster.py -g /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/Orthogroups.txt --cluster_ids /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/test.kinfin_results/all/single_copy.cluster_ids.txt -s
+ 
 
-"[+] Start ...
-[+] Parsing clusters in /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/test.kinfin_results/all/single_copy.cluster_ids.txt ...
-[+] Parse groups /Volumes/archive/kennylab/roseanna_kawakawa/kinfin/blastpipeline/Orthogroups.txt ...
-[+] Found 100% of clusters ...
-[+] Writing files ...
-"
-
-# backup
-
-cp *.txt blastpipeline
